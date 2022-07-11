e5367a805a85a6f6aa80bbfa6120106d45703eb2
Pedro henrique <pedro.henrique@agenciam3.com>
2022-07-11 18:57:35 -0300

    feat(answers)

--- resposta_questao_2.js
+++ resposta_questao_2.js
@@ -1,1 +1,11 @@
+var input = [4, 5, 44, 98, 4, 5, 6, 7];
 
+const checkOrderNumber = (arr) => {
+  const filteredArray = arr.filter((ant, pos) => {
+    return arr.indexOf(ant) !== pos;
+  });
+
+  return filteredArray;
+};
+
+console.log("Output", checkOrderNumber(input));
